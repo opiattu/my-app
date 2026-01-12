7547694685036f0ce69f17ada768bed1f8d42bf8
opiattu <154826443+opiattu@users.noreply.github.com>
2026-01-13 01:34:14 +0300

    Commit to main

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-app/', 
-})
+  base: "/my-app/",
+});
